12bb955696e8d9232cbeb055190ec9cd9561faa0
dorlevy <ddl105095@gmail.com>
2020-04-12 22:21:53 +0300

    new angular

--- traveltour/karma.conf.js
+++ traveltour/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/TravelTour'),
+      dir: require('path').join(__dirname, './coverage/traveltour'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
