f4696cd0d277f80c7f8265f5f894e45d499892d7
reouven benitah <reouvenbenitah@h-MacBook-Pro-sl-reouven.local>
2020-04-22 11:55:25 +0300

    change accunt firebase

--- traveltour/src/environments/environment.prod.ts
+++ traveltour/src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyAyNuoixvSyPryeckXjULaWtk3ec40bKZg',
-    authDomain: 'traveltour-c859c.firebaseapp.com',
-    databaseURL: 'https://traveltour-c859c.firebaseio.com',
-    projectId: 'traveltour-c859c',
-    storageBucket: 'traveltour-c859c.appspot.com',
-    messagingSenderId: '801067732589',
-    appId: '1:801067732589:web:e0e8e75fa556558a607964',
-    measurementId: 'G-10307DLN88',
-  },
+
+  firebase:{
+    apiKey: "AIzaSyAq6nwSI2SJUnAVRRNuUl44Sn3vBdRGfGk",
+    authDomain: "traveltour-4dc76.firebaseapp.com",
+    databaseURL: "https://traveltour-4dc76.firebaseio.com",
+    projectId: "traveltour-4dc76",
+    storageBucket: "traveltour-4dc76.appspot.com",
+    messagingSenderId: "300386061218",
+    appId: "1:300386061218:web:b84badabaa034c30ddc0fb",
+    measurementId: "G-FSYE31QB9N"
+  }
 };
