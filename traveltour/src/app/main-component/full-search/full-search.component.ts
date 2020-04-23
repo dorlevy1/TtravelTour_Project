a3728d8a427737b4bf45fe070f38d975bf7466eb
dorlevy1 <ddl105095@gmail.com>
2020-04-23 11:10:58 +0300

    sass

--- traveltour/src/app/main-component/full-search/full-search.component.ts
+++ traveltour/src/app/main-component/full-search/full-search.component.ts
@@ -18,7 +18,7 @@ export class FullSearchComponent implements OnInit {
       $("#datepicker").datepicker({
         prevText: '<i class="fa fa-fw fa-angle-left"></i>',
         nextText: '<i class="fa fa-fw fa-angle-right"></i>',
-      });
+      }); //
     });
     $(function () {
       $("#datepicker2").datepicker({
